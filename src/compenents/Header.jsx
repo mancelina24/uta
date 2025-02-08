7ce6b04f24e705ff811b98e40516ec5dc78469f2
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-09 00:19:17 +0300

    17

--- src/compenents/Header.jsx
+++ src/compenents/Header.jsx
@@ -15,61 +15,55 @@ const Header = () => {
     useContext(UserContext);
 
   return (
-    <header>
-      <div className="flex flex-col">
-        <div>
-          <div>
-            <img src={logo} className="w-45 h-auto ml-5 mt-5" alt="" />
-          </div>
-          <div className="relative">
-            <div className=" absolute flex bottom-15 left-280  bg-none border-none ">
-              <div className="flex flex-row cursor-pointer pr-4 font-bold  ">
-                <Switch
-                  {...label}
-                  checked={darkMode}
-                  onChange={toggleTheme}
-                  sx={{
-                    width: 55,
-                    height: 24,
-                    padding: 0,
-                    display: "flex",
-                    "& .MuiSwitch-thumb": {
-                      backgroundColor: "#f7c75e",
-                      width: 15, // Thumb (circle) width
-                      height: 16, // Thumb (circle) height
-                      transform: "translateY(-6px)",
-                    },
+    <header className="w-full">
+      <div className="flex justify-around">
+        <div className="w-[15%]">
+          <img src={logo} className="w-45 h-auto ml-5 mt-5" alt="" />
+        </div>
+        <div className="w-[80%]">
+          <NavlinkMenu />
+        </div>
+        <div className="mt-10 w-[25%]">
+          <div className="flex bottom-15 bg-none border-none ">
+            <div className="flex flex-row cursor-pointer pr-4 font-bold  ">
+              <Switch
+                {...label}
+                checked={darkMode}
+                onChange={toggleTheme}
+                sx={{
+                  width: 40,
+                  height: 24,
+                  padding: 0,
+                  display: "flex",
+                  "& .MuiSwitch-thumb": {
+                    backgroundColor: "#f7c75e",
+                    width: 13, // Thumb (circle) width
+                    height: 16, // Thumb (circle) height
+                    transform: "translateY(-6px)",
+                  },
 
-                    "& .MuiSwitch-track": {
-                      backgroundColor: darkMode ? "red" : "black",
-                      borderRadius: "9999px", // Keep the track rounded
-                      width: 55, // Thumb (circle) width
-                      height: 24, // Thumb (circle) height
-                    },
-                  }}
-                />
-                <label className="ml-2">{themeName}</label>
-              </div>
-              <span className="font-bold pr-4"> | </span>
-              <div className="flex flex-row">
-                <label onClick={languageChange}>
-                  <span className="text-[#f7c75e] font-bold">
-                    {language === TURKCE
-                      ? languageEng.tr + languageEng.ekTr
-                      : ""}
-                  </span>
-                  <span className="text-[##f7c75e] font-bold">
-                    {language === TURKCE
-                      ? ""
-                      : languageTr.ekEng + languageTr.eng}
-                  </span>
-                </label>
-              </div>
+                  "& .MuiSwitch-track": {
+                    backgroundColor: darkMode ? "red" : "black",
+                    borderRadius: "9999px", // Keep the track rounded
+                    width: 55, // Thumb (circle) width
+                    height: 24, // Thumb (circle) height
+                  },
+                }}
+              />
+              <label className=" ml-2 mt-[0.4rem] text-xs">{themeName}</label>
+            </div>
+            <span className="font-bold pr-2"> | </span>
+            <div className="flex flex-row">
+              <label onClick={languageChange}>
+                <span className="text-[#f7c75e] text-xs font-bold">
+                  {language === TURKCE ? languageEng.tr + languageEng.ekTr : ""}
+                </span>
+                <span className="text-[##f7c75e] text-xs font-bold">
+                  {language === TURKCE ? "" : languageTr.ekEng + languageTr.eng}
+                </span>
+              </label>
             </div>
           </div>
-        </div>
-        <div>
-          <NavlinkMenu />
         </div>
       </div>
     </header>
--- src/compenents/NavlinkMenu.jsx
+++ src/compenents/NavlinkMenu.jsx
@@ -1,58 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import { TURKCE } from "../api/dataTr.js";
 
 export const NavlinkMenu = () => {
+  const { language, darkMode } = useContext(UserContext);
   return (
-    <div>
-      <nav>
-        <ul className="flex flex-row justify-center gap-10 mt-5 mr-20 font-bold ">
-          <li>
-            <NavLink className="hover:bg-[#f7c75e] px-4 py-2 rounded-md" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
-              to="/projects"
-            >
-              Projects
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
-              to="/ourservices"
-            >
-              Our Services
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
-              to="/galery"
-            >
-              Galery
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
-              to="/about"
-            >
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
-              to="/contact"
-            >
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className=" mt-3">
+      <ul className="flex flex-row justify-center gap-10  font-bold py-8 px-6 ">
+        <li>
+          <NavLink className="hover:bg-[#f7c75e]  px-4 py-2 rounded-md" to="/">
+            {language === TURKCE ? "Home" : "Ana Sayfa"}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
+            to="/projects"
+          >
+            {language === TURKCE ? "Projects" : "Projeler"}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
+            to="/ourservices"
+          >
+            {language === TURKCE ? "Our Services" : "Hizmetlerimiz"}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
+            to="/galery"
+          >
+            {language === TURKCE ? "Gallery" : "Galeri"}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
+            to="/about"
+          >
+            {language === TURKCE ? "About" : "Hakkımızda"}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="hover:bg-[#f7c75e] px-4 py-2 rounded-md"
+            to="/contact"
+          >
+            {language === TURKCE ? "Contact " : "İletişim"}
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
